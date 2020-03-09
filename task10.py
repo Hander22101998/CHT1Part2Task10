d5a9b9d9c7a45f39f333e827c4d29f106c64c81d
Hander22101998 <hander22101998@gmail.com>
2020-03-09 16:19:21 +0600

    tenth commit

---
+++ task10.py
@@ -0,0 +1,20 @@
+a = "In the first line, print the third character of this string."
+print (a [3])
+b = "In the second line, print the second to last character of this string."
+print (b[1 : -1])
+c = "In the third line, print the first five characters of this string."
+print(c [0 : 5])
+d = "In the fourth line, print all but the last two characters of this string."
+print(d[ :-3])
+e = "In the fifth line, print all the characters of this string with even indices"
+print(e[::2])
+f = "remember indexing starts at 0, so the characters are displayed starting with the first"
+(f[1::2])
+j = "In the sixth line, print all the characters of this string with odd indices i.e. starting with the second character in the string"
+print (j[1::3]) 
+l = "In the seventh line, print all the characters of the string in reverse order."
+print (l[::-1])
+o = "In the eighth line, print every second character of the string in reverse order,starting from the last one." 
+print (o[::-2])
+k  = "In the ninth line, print the length of the given string."
+print(len(k))
